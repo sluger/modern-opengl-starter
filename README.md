5e3ba47f1bc8b91b4cda507642358d8450d5588a
Stefan Luger <stefan.luger@hexagon.com>
2024-02-08 17:18:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# modern-opengl-starter
+
+Based on https://github.com/ArthurSonzogni/OpenGL_CMake_Skeleton
+
+// TODO:
+Install clang with brew?
+
+```
+git clone --recursive git@github.com:ArthurSonzogni/OpenGL_CMake_Skeleton.git
+cd modern-opengl-starter
+mkdir build
+cd build
+cmake ..
+make -j
+./opengl-cmake-skeleton
+```
---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// window dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, const char *argv[])
+{
+
+  // init GLFW
+  if (!glfwInit())
+  {
+    printf("GLFW init failed!");
+    glfwTerminate();
+    return 1;
+  }
+
+  // setup GLFW window properties
+  // openGL version
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  // core profile = no backwards compatibility
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // allow forward compatibility
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+
+  if (!mainWindow)
+  {
+    printf("GLFW window creation failed!");
+    glfwTerminate();
+    return 1;
+  }
+
+  // get buffer size information
+  int bufferWidth, bufferHeight;
+  glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+  // set context for GLEW to use
+  glfwMakeContextCurrent(mainWindow);
+
+  // allow modern extension features
+  glewExperimental = GL_TRUE;
+
+  if (glewInit() != GLEW_OK)
+  {
+    printf("GLEW init failed!");
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+    return 1;
+  }
+
+  // setup viewport size
+  glViewport(0, 0, bufferWidth, bufferHeight);
+
+  // loop until window closed
+  while (glfwWindowShouldClose(mainWindow))
+  {
+    // get and handle user input events
+    glfwPollEvents();
+
+    // clear window
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(mainWindow);
+  }
+
+  return 0;
+}
